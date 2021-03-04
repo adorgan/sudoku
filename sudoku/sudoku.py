3857b88504d30c9deeeb75015c4b5da6e0d4bd7b
Andrew Dorgan <adorgan@gmail.com>
2021-03-03 20:27:55 -0600

    reworked init

---
+++ sudoku/sudoku.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from algorithms import validateSolution
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/verify', methods=['POST'])
+def verify():
+    vals = list(request.form.to_dict().values())
+    if '' in vals:
+        return 'Sorry, Try Again.'
+
+    print(len(vals))
+    grid = []
+    row = []
+    for i in range(len(vals)):
+        if i != 0 and i % 9 == 0:
+            grid.append(row)
+            row = []
+            row.append(int(vals[i]))
+        else:
+            row.append(int(vals[i]))
+    grid.append(row)
+
+    if validateSolution(grid):
+        return 'Correct!'
+    else:
+        return 'Sorry, Try Again.'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
